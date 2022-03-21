03623660d38b615c3f6da34594564f806185fda5
Antonio Sabino <aslimajr@outlook.com>
2022-03-21 20:50:51 -0300

    Logica do jogo

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32 || event.keyCode === 38) {
@@ -10,12 +12,10 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-  let position = 0;
-
   isJumping = true;
 
   let upInterval = setInterval(() => {
-    if (position >= 150) {
+    if (position >= 180) {
       clearInterval(upInterval);
 
       let downInterval = setInterval(() => {
@@ -34,4 +34,30 @@ function jump() {
   }, 20);
 }
 
+function createCactus() {
+  const cactus = document.createElement("div");
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 4000;
+
+  cactus.classList.add("cactus");
+  cactus.style.left = `${cactusPosition}px`;
+  background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = `${cactusPosition}px`;
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 document.addEventListener("keyup", handleKeyUp);
